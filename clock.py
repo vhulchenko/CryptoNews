85741323824564c64667d4ce1d688c17389d15db
Vladimir Hulchenko <volodymyr.hulchenko@customertimes.com>
2021-11-05 16:35:57 +0200

    added coin job

--- clock.py
+++ clock.py
@@ -1,9 +1,11 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
+from coin import jobs
 
 sched = BlockingScheduler()
 
 @sched.scheduled_job('interval', minutes=3)
 def timed_job():
+    jobs.countSeconds()
     print('This job is run every three minutes.')
 
 @sched.scheduled_job('cron', day_of_week='mon-fri', hour=17)
